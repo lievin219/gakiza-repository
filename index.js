39081bec6203cef99264985013b80e6e7cfe96fd
lievin219 <gakizalievin219@gmail.com>
2024-02-28 11:09:58 +0200

    addition of javascript calculator

---
+++ index.js
@@ -0,0 +1,26 @@
+ let firstnumber=document.getElementById("first")
+ let secondnumber=document.getElementById("second")
+ let buttoni=document.getElementById('button')
+ let result=document.getElementById('result')
+let addition=document.getElementById("add")
+let substraction=document.getElementById("substract")
+let diviSION=document.getElementById("divide")
+let multiplication =document.getElementById('multiply')
+
+    function additioning(){
+         result.innerText=firstnumber+secondnumber
+    }
+    function substact(){
+        result.innerText=firstnumber-secondnumber
+   }
+   function multiply(){
+    result.innerText=firstnumber*secondnumber
+}
+function divisioning(){
+    result.innerText=firstnumber/secondnumber
+}
+    
+addition.addEventListener('click',additioning)
+multiplication.addEventListener('click',multiply)
+substraction.addEventListener('click',substact)
+ diviSION.addEventListener('click',divisioning)
